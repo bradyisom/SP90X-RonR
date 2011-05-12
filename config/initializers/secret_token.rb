8eaf3a3ae8d07133ce65db9e4ba98271645f756d
Brady Isom <bradyisom@gmail.com>
2011-05-12 15:33:30 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SP90X::Application.config.secret_token = '9fcefd852a43f32705dd584bdfe4b93c8111f370b0a0fea75fe193d162afe93041994ff6643f0e9e69d07b7995f0d0c7c273c4324e1e91cdf478e7ed6c32013d'
