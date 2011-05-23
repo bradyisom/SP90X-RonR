35e27bc15428637301a75863038d3d76d652850a
Brady Isom <bradyisom@gmail.com>
2011-05-23 15:50:37 -0600

    Encrypted user passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110512225310
+# Schema version: 20110523183716
 #
 # Table name: users
 #
@@ -12,7 +12,10 @@
 #  gender             :string(1)
 #  created_at         :datetime
 #  updated_at         :datetime
+#  salt               :string(255)
 #
+
+require 'digest'
 
 class User < ActiveRecord::Base
   attr_accessor :password
@@ -37,10 +40,55 @@ class User < ActiveRecord::Base
   validates :password,    :presence => true,
                           :confirmation => true,
                           :length => { :within => 6..40 }
+                          
+  before_save :encrypt_password
+
+  def has_password?(password)
+    if salt.blank?
+      correct = (encrypted_password == md5_hash(password))
+      upgrade_password(password) if correct
+      correct
+    else
+      encrypted_password == encrypt(password)
+    end
+  end
+  
+  def self.authenticate(username, submittedPassword)
+    user = find_by_username(username)
+    return nil if user.nil?
+    return user if user.has_password?(submittedPassword)
+  end
 
 private
   def encrypt_password
-    
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
+  end
+
+  def upgrade_password(newPassword)
+    if salt.blank?
+      self.password = newPassword
+      self.password_confirmation = newPassword
+      self.salt = make_salt
+      self.encrypted_password = encrypt(newPassword)
+      save(:validate => false)
+    end
+  end
+  
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+  
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+  
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
+  
+  def md5_hash(string)
+    Digest::MD5.hexdigest(string)
   end
     
 end
